ec6c9b2d0cbd05e82c2e353a10ee302e294cfa8b
Vikas Sonawane <vikassonawane@gmail.com>
2026-03-13 20:45:31 +0530

    testing

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4099900166"
+    serviceWorkerVersion: "2073110510"
   }
 });
